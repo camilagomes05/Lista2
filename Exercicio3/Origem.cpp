7613866168360de22a3324f9da5e89332ab40fa6
camilagomes05 <camilagomes05@edu.unisinos.br>
2021-09-19 22:25:12 -0300

    lista2

---
+++ Exercicio3/Origem.cpp
@@ -0,0 +1,3 @@
+// Resposta O objeto desenhado na tela diminuiu de tamanho, ocupando o tamnho de um pixel, anterando a posição do y, 
+//ou seja, tudo que adicionar na posição 0.0, tudo que for desenhado vai ser no y positivo
+
